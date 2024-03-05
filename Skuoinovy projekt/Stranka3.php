40b33c8b17eecf8031ca9f50655ebb2ed6ed72c4
Tomesko <117730575+Tomesko@users.noreply.github.com>
2024-03-05 12:38:34 +0100

    Add files via upload

---
+++ Skuoinovy projekt/Stranka3.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="Hotbar.css">
+
+</head>
+<body>
+
+<?php
+include("Hotbar.html");
+?>
+
+<div style="padding:20px;margin-top:30px;background-color:white;height:150px;">
+  <h1>planety</h1>
+
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+ 
+</div>
+
+<div style="overflow-x: auto;">
+  <table>
+    <tr>
+      <th>Name</th>
+        <th>Rare structure</th>
+        <th>Hazard lvl</th>
+        <th>Wind lvl</th>
+        <th>Rare items</th>
+        <th>N. caves</th>
+        <th>Common sources</th>
+        <th>Earth lvl</th>
+        <th>Sun lvl</th>
+        <th>N. flowers</th>
+        <th>Colors</th>
+    </tr>
+    <tr>
+      <td>Atrox</td>
+        <td>50</td>
+        <td>5</td>
+        <td>28</td>
+        <td>120</td>
+        <td>1</td>
+        <td>3</td>
+        <td>18</td>
+        <td>64</td>
+        <td>5</td>
+        <td>12</td>
+    </tr>
+    <tr>
+      <td>Phobos</td>
+      <td>40</td>
+      <td>4</td>
+      <td>97</td>
+      <td>10</td>
+      <td>940</td>
+      <td>48</td>
+      <td>9</td>
+      <td>94</td>
+      <td>04</td>
+      <td>74</td>
+    </tr>
+    <tr>
+      <td>Sylva</td>
+      <td>8</td>
+      <td>7</td>
+      <td>6</td>
+      <td>57</td>
+      <td>68</td>
+      <td>4</td>
+      <td>9</td>
+      <td>96</td>
+      <td>67</td>
+      <td>69</td>
+    </tr>
+  </table>
+</div>
+
+
+</body>
+</html>
